9ee717843bfefe003afd2229509d8b27890c04f5
Aryans-github <150815102+Aryans-github@users.noreply.github.com>
2025-12-22 20:51:19 +0530

    Time: 2 ms (47.37%) | Memory: 94.5 MB (20.37%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -2,45 +2,45 @@ class Solution {
     public static int maxProfit(int[] prices) {
         
         
-        int minValue = Integer.MAX_VALUE;
-        int maxValue =0;
+        // int minValue = Integer.MAX_VALUE;
+        // int maxValue =0;
         
-        int profit =0;
+        // int profit =0;
        
-        for(int i =0;i<prices.length;i++){
-            if(prices[i]<minValue){
-                minValue=prices[i];
+        // for(int i =0;i<prices.length;i++){
+        //     if(prices[i]<minValue){
+        //         minValue=prices[i];
                 
-            }
-            profit = prices[i]-minValue;
-            if(profit>maxValue) maxValue=profit;
-            // maxValue = Math.max(profit,maxValue);
-        }
-        
-        return maxValue;
-
-
-        // if (prices.length==0) {
-        //     return 0;
-        // }
-        // int left = 0;
-        // int right = left + 1;
-        // int mprofit = 0;
-
-        // while (right < prices.length) {
-        //     if (prices[left] < prices[right]) {
-        //        int profit = prices[right] - prices[left];
-        //         mprofit = Math.max(profit, mprofit);
-                
-
-        //     } else {
-        //         left = right;
-               
         //     }
-        //     right++;
+        //     profit = prices[i]-minValue;
+        //     if(profit>maxValue) maxValue=profit;
+        //     // maxValue = Math.max(profit,maxValue);
         // }
+        
+        // return maxValue;
 
-        // return mprofit;
+
+        if (prices.length==0) {
+            return 0;
+        }
+        int left = 0;
+        int right = left + 1;
+        int mprofit = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+               int profit = prices[right] - prices[left];
+                mprofit = Math.max(profit, mprofit);
+                
+
+            } else {
+                left = right;
+               
+            }
+            right++;
+        }
+
+        return mprofit;
 
     }
 }
