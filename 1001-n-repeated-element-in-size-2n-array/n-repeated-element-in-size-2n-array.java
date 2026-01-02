134fd967226129fa1936e86eb12af6ef5eed9063
Aryan Jumde <aryanjumde2004@gmail.com>
2026-01-02 20:01:38 +0530

    Time: 3 ms (48.08%) | Memory: 45.6 MB (100.00%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,12 +1,31 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        HashMap<Integer, Integer> hs = new HashMap<>();
-        for (int i : nums) {
-            hs.put(i, hs.getOrDefault(i, 0) + 1);
+
+        int [] counts = new int [10001];
+
+        for(int i:nums){
+            counts[i]++;
         }
 
-        int maxKey = Collections.max(hs.entrySet(), Map.Entry.comparingByValue()).getKey();
+        int maxCount=-1;
+        int value=0;
+        for(int i=0;i<counts.length;i++){
+            if(counts[i]>maxCount){
+                maxCount=counts[i];
+                value=i;
+            }
 
-        return maxKey;
+        }
+
+        return value;
+
+        // HashMap<Integer, Integer> hs = new HashMap<>();
+        // for (int i : nums) {
+        //     hs.put(i, hs.getOrDefault(i, 0) + 1);
+        // }
+
+        // int maxKey = Collections.max(hs.entrySet(), Map.Entry.comparingByValue()).getKey();
+
+        // return maxKey;
     }
 }
