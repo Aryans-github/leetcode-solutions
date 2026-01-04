b8d96a29f7af1fff70a62abc3c46e923a8dfa17b
Aryan Jumde <aryanjumde2004@gmail.com>
2026-01-04 19:22:54 +0530

    Time: 6 ms (67.20%) | Memory: 46.2 MB (52.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,17 +1,48 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int maxLength = 0;
-        int left = 0; // Left pointer of the window
-        HashSet<Character> set = new HashSet<>();
+        if (s.length() == 0)
+            return 0;
+        if (s.length() == 1)
+            return 1;
 
-        for (int right = 0; right < s.length(); right++) {
-            while (set.contains(s.charAt(right))) {
-                set.remove(s.charAt(left)); // Shrink the window
-                left++;
+        HashMap<Character, Integer> hs = new HashMap<>();
+        int l = 0, r = 0;
+        int maxLength = 0;
+
+        while (r < s.length()) {
+
+            if (hs.containsKey(s.charAt(r))) {
+                if (hs.get(s.charAt(r)) >= l) {
+                    l = hs.get(s.charAt(r)) + 1;
+                    hs.put(s.charAt(r), r);
+                    
+
+                }
+                else{
+                    hs.put(s.charAt(r),r);
+                }
+            } else {
+                hs.put(s.charAt(r), r);
+                
             }
-            set.add(s.charAt(right)); // Expand the window
-            maxLength = Math.max(maxLength, right - left + 1);
+            maxLength = Math.max(r - l + 1, maxLength);
+            r++;
         }
+
         return maxLength;
+
+        // int maxLength = 0;
+        // int left = 0; // Left pointer of the window
+        // HashSet<Character> set = new HashSet<>();
+
+        // for (int right = 0; right < s.length(); right++) {
+        //     while (set.contains(s.charAt(right))) {
+        //         set.remove(s.charAt(left)); // Shrink the window
+        //         left++;
+        //     }
+        //     set.add(s.charAt(right)); // Expand the window
+        //     maxLength = Math.max(maxLength, right - left + 1);
+        // }
+        // return maxLength;
     }
 }
