ab5825314a3bb5a39c0a63c90294ca329399cce3
Aryan Jumde <aryanjumde2004@gmail.com>
2026-01-08 22:28:12 +0530

    Time: 7 ms (35.46%) | Memory: 46.3 MB (47.07%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,33 +1,53 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0)
-            return 0;
-        if (s.length() == 1)
-            return 1;
 
-        HashMap<Character, Integer> hs = new HashMap<>();
-        int l = 0, r = 0;
-        int maxLength = 0;
 
-        while (r < s.length()) {
+        int l=0,r=0,maxLength=0;
 
-            if (hs.containsKey(s.charAt(r))) {
-                if (hs.get(s.charAt(r)) >= l) {
-                    l = hs.get(s.charAt(r)) + 1;
-                    hs.put(s.charAt(r), r);
-
-                } else {
-                    hs.put(s.charAt(r), r);
-                }
-            } else {
-                hs.put(s.charAt(r), r);
-
+        HashSet<Character> hs = new HashSet<>();
+        while(r<s.length()){
+            if(!hs.contains(s.charAt(r))){
+                hs.add(s.charAt(r));
+                r++;
+                maxLength= Math.max(maxLength,hs.size());
             }
-            maxLength = Math.max(r - l + 1, maxLength);
-            r++;
+            else{
+                hs.remove(s.charAt(l));
+                l++;
+            }
         }
-
         return maxLength;
+
+        // Using HashMap
+
+        // if (s.length() == 0)
+        //     return 0;
+        // if (s.length() == 1)
+        //     return 1;
+
+        // HashMap<Character, Integer> hs = new HashMap<>();
+        // int l = 0, r = 0;
+        // int maxLength = 0;
+
+        // while (r < s.length()) {
+
+        //     if (hs.containsKey(s.charAt(r))) {
+        //         if (hs.get(s.charAt(r)) >= l) {
+        //             l = hs.get(s.charAt(r)) + 1;
+        //             hs.put(s.charAt(r), r);
+
+        //         } else {
+        //             hs.put(s.charAt(r), r);
+        //         }
+        //     } else {
+        //         hs.put(s.charAt(r), r);
+
+        //     }
+        //     maxLength = Math.max(r - l + 1, maxLength);
+        //     r++;
+        // }
+
+        // return maxLength;
 
         // int maxLength = 0;
         // int left = 0; // Left pointer of the window
