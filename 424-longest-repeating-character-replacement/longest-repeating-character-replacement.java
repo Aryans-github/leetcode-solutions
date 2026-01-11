89b3992dc9c12407ef4d7ea155e7f05e98882911
Aryan Jumde <aryanjumde2004@gmail.com>
2026-01-11 19:57:08 +0530

    Time: 15 ms (39.05%) | Memory: 46.4 MB (34.98%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,35 +1,61 @@
 class Solution {
     public int characterReplacement(String s, int k) {
 
-
         int l=0,r=0,maxLength=0;
-        HashMap<Character,Integer> hs = new HashMap<>();
+        int[] hash = new int[26];
+        int maxFreq=0;
         while(r<s.length()){
-            hs.put(s.charAt(r),hs.getOrDefault(s.charAt(r),0)+1);
 
-            int maxFreq = 0;
-            for(int freq : hs.values()){
-                maxFreq = Math.max(maxFreq, freq);
+            hash[s.charAt(r)-'A']++;
+
+            for(int freq:hash){
+                maxFreq= Math.max(freq,maxFreq);
             }
-
-            int replacements=(r-l+1)-maxFreq;
-            if(replacements>k){
-                hs.put(s.charAt(l),hs.get(s.charAt(l))-1);
-                if(hs.get(s.charAt(l))==0){
-                    hs.remove(s.charAt(l));
-                }
+            
+            int replacements = (r-l+1)-maxFreq;
+            while(replacements>k){
+                hash[s.charAt(l)-'A']--;
                 l++;
-                
                 maxFreq = 0;
-                for(int freq : hs.values()){
-                    maxFreq = Math.max(maxFreq, freq);
+                for(int freq:hash){
+                    maxFreq= Math.max(freq,maxFreq);
                 }
-                replacements=(r-l+1)-maxFreq;
+                replacements = (r-l+1)-maxFreq;
             }
             maxLength=Math.max(maxLength,r-l+1);
             r++;
         }
         return maxLength;
+        // Correct but runtime is high , due to use of hashmap
+
+        // int l=0,r=0,maxLength=0;
+        // HashMap<Character,Integer> hs = new HashMap<>();
+        // while(r<s.length()){
+        //     hs.put(s.charAt(r),hs.getOrDefault(s.charAt(r),0)+1);
+
+        //     int maxFreq = 0;
+        //     for(int freq : hs.values()){
+        //         maxFreq = Math.max(maxFreq, freq);
+        //     }
+
+        //     int replacements=(r-l+1)-maxFreq;
+        //     if(replacements>k){
+        //         hs.put(s.charAt(l),hs.get(s.charAt(l))-1);
+        //         if(hs.get(s.charAt(l))==0){
+        //             hs.remove(s.charAt(l));
+        //         }
+        //         l++;
+                
+        //         maxFreq = 0;
+        //         for(int freq : hs.values()){
+        //             maxFreq = Math.max(maxFreq, freq);
+        //         }
+        //         replacements=(r-l+1)-maxFreq;
+        //     }
+        //     maxLength=Math.max(maxLength,r-l+1);
+        //     r++;
+        // }
+        // return maxLength;
 
         // Brute force but TLE due to big input.
 
