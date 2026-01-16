ff25b1f738fe9a2b0bbbb090f63a57e1ce5e9e58
Aryan Jumde <aryanjumde2004@gmail.com>
2026-01-17 01:36:10 +0530

    Time: 4 ms (42.87%) | Memory: 43.7 MB (12.88%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
+                if (stk.isEmpty() || stk.peek() != s.charAt(i)) {
+                    return false; 
+                }
+                stk.pop(); 
+            }
+            if (s.charAt(i) == '(') {
+                stk.push(')');
+            } else if (s.charAt(i) == '{') {
+                stk.push('}');
+            } else if (s.charAt(i) == '[') {
+                stk.push(']');
+            }
+            // if (!stk.isEmpty()) {
+            //     if (stk.peek() == s.charAt(i)) {
+            //         stk.pop();
+            //     }
+
+            // }
+        }
+        if (stk.isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
