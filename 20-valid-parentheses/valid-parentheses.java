7f1843f26c3d1be1499e998f591e95abaa393764
Aryan Jumde <aryanjumde2004@gmail.com>
2026-01-17 16:22:28 +0530

    Time: 4 ms (42.87%) | Memory: 43.7 MB (12.88%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -3,18 +3,23 @@ import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
 
+        // Inserting the corresponding close bracket when an open bracket is identified    
+
         Stack<Character> stk = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
-            
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
-                stk.push(s.charAt(i));
-            } else {
-                if (stk.isEmpty())
-                    return false;
-                if (s.charAt(i) == ')' && stk.peek() == '(' || s.charAt(i) == '}' && stk.peek() == '{' || s.charAt(i) == ']' && stk.peek() == '[') {
-                    stk.pop();
-                } else
-                    return false;
+            if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
+                if (stk.isEmpty() || stk.peek() != s.charAt(i)) {
+                    return false; 
+                }
+                stk.pop(); 
+            }
+            if (s.charAt(i) == '(') {
+                stk.push(')');
+            } else if (s.charAt(i) == '{') {
+                stk.push('}');
+            } else if (s.charAt(i) == '[') {
+                stk.push(']');
             }
 
         }
@@ -23,21 +28,20 @@ class Solution {
         else
             return false;
 
-        // Stack<Character> stk = new Stack<>();
+        // // Inserting the open bracket to stack
 
+        // Stack<Character> stk = new Stack<>();
         // for (int i = 0; i < s.length(); i++) {
-        //     if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
-        //         if (stk.isEmpty() || stk.peek() != s.charAt(i)) {
-        //             return false; 
-        //         }
-        //         stk.pop(); 
-        //     }
-        //     if (s.charAt(i) == '(') {
-        //         stk.push(')');
-        //     } else if (s.charAt(i) == '{') {
-        //         stk.push('}');
-        //     } else if (s.charAt(i) == '[') {
-        //         stk.push(']');
+
+        //     if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+        //         stk.push(s.charAt(i));
+        //     } else {
+        //         if (stk.isEmpty())
+        //             return false;
+        //         if (s.charAt(i) == ')' && stk.peek() == '(' || s.charAt(i) == '}' && stk.peek() == '{' || s.charAt(i) == ']' && stk.peek() == '[') {
+        //             stk.pop();
+        //         } else
+        //             return false;
         //     }
 
         // }
@@ -45,5 +49,7 @@ class Solution {
         //     return true;
         // else
         //     return false;
+
+        
     }
 }
