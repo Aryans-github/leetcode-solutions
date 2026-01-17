7c762ae6e49989936de7801d78366f7d1a214742
Aryan Jumde <aryanjumde2004@gmail.com>
2026-01-17 16:20:14 +0530

    Time: 5 ms (17.18%) | Memory: 43.9 MB (6.97%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,27 +2,48 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stk = new Stack<>();
 
+        Stack<Character> stk = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
-                if (stk.isEmpty() || stk.peek() != s.charAt(i)) {
-                    return false; 
-                }
-                stk.pop(); 
-            }
-            if (s.charAt(i) == '(') {
-                stk.push(')');
-            } else if (s.charAt(i) == '{') {
-                stk.push('}');
-            } else if (s.charAt(i) == '[') {
-                stk.push(']');
-            }
             
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stk.push(s.charAt(i));
+            } else {
+                if (stk.isEmpty())
+                    return false;
+                if (s.charAt(i) == ')' && stk.peek() == '(' || s.charAt(i) == '}' && stk.peek() == '{' || s.charAt(i) == ']' && stk.peek() == '[') {
+                    stk.pop();
+                } else
+                    return false;
+            }
+
         }
         if (stk.isEmpty())
             return true;
         else
             return false;
+
+        // Stack<Character> stk = new Stack<>();
+
+        // for (int i = 0; i < s.length(); i++) {
+        //     if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
+        //         if (stk.isEmpty() || stk.peek() != s.charAt(i)) {
+        //             return false; 
+        //         }
+        //         stk.pop(); 
+        //     }
+        //     if (s.charAt(i) == '(') {
+        //         stk.push(')');
+        //     } else if (s.charAt(i) == '{') {
+        //         stk.push('}');
+        //     } else if (s.charAt(i) == '[') {
+        //         stk.push(']');
+        //     }
+
+        // }
+        // if (stk.isEmpty())
+        //     return true;
+        // else
+        //     return false;
     }
 }
