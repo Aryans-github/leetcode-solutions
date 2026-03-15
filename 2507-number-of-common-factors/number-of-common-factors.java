19176c4d79d355fdf2f366d1b0c00fe98d7b3b32
Aryan Jumde <aryanjumde2004@gmail.com>
2026-03-15 23:28:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (72.14%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.java
+++ 2507-number-of-common-factors/number-of-common-factors.java
@@ -1,7 +1,8 @@
 class Solution {
     public int commonFactors(int a, int b) {
         int factors=0;
-        for(int i=1;i<=Math.min(a,b);i++){
+        int iterate=Math.min(a,b);
+        for(int i=1;i<=iterate;i++){
             if(a%i==0 && b%i==0) factors++;
         }
         return factors;
