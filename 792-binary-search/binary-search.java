eb9603797ec2676f4a8f158f2c2ba05f8fcd9e2d
Aryan Jumde <aryanjumde2004@gmail.com>
2026-03-21 21:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 48.7 MB (10.07%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,20 +1,35 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int first = 0;
-        int last = nums.length-1;
+        // Using Recursion
 
-        while(first<=last){
+        return binaryS(nums,target,0,nums.length-1);
 
-            int mid = (first+last)/2;
-            if(target==nums[mid]) return mid;
-            else if(target>nums[mid]){
-                first=mid+1;
-            }
-            else if(target<nums[mid]){
-                last=mid-1;
-            }
+
+        // Basic Method
+
+        // int first = 0;
+        // int last = nums.length-1;
+
+        // while(first<=last){
+
+        //     int mid = (first+last)/2;
+        //     if(target==nums[mid]) return mid;
+        //     else if(target>nums[mid]){
+        //         first=mid+1;
+        //     }
+        //     else if(target<nums[mid]){
+        //         last=mid-1;
+        //     }
             
-        }
-        return -1;
+        // }
+        // return -1;
+    }
+
+    public int binaryS(int[] arr,int target,int first,int last){
+        if(last<first) return -1;
+        int mid = (first+last)/2;
+        if(arr[mid]==target) return mid;
+        if(target>arr[mid]) return binaryS(arr,target,mid+1,last);
+        else return binaryS(arr,target,first,mid-1);
     }
 }
