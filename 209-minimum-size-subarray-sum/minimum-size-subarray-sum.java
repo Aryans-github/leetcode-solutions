603c0d355c427e3d6569720dd18f2a891f508153
Aryan Jumde <aryanjumde2004@gmail.com>
2026-07-26 18:09:40 +0530

    Time: 1 ms (99.94%) | Memory: 69 MB (63.27%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,19 +1,58 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left = 0;
-        int sum = 0;
-        int ans = Integer.MAX_VALUE;
-        for (int right = 0; right < nums.length; right++) {
-            sum += nums[right];
 
-            while (sum >= target) {
-                ans = Math.min(ans, right - left + 1);
-                sum -= nums[left];
+        int left=0;
+        int right=0;
+        int sum =0;
+        int minLen=Integer.MAX_VALUE;
+        while(right<nums.length && left<=right){
+            sum = sum + nums[right];
+            while(sum>=target){
+                minLen=Math.min(minLen,right-left+1);
+                sum-=nums[left];
                 left++;
+                // right++;
             }
+            right++;
 
         }
-        if(ans==Integer.MAX_VALUE) return 0;
-        return ans;
+        if(minLen==Integer.MAX_VALUE) return 0;
+        return minLen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int left = 0;
+        // int sum = 0;
+        // int ans = Integer.MAX_VALUE;
+        // for (int right = 0; right < nums.length; right++) {
+        //     sum += nums[right];
+        //     while (sum >= target) {
+        //         ans = Math.min(ans, right - left + 1);
+        //         sum -= nums[left];
+        //         left++;
+        //     }
+        // }
+        // if(ans==Integer.MAX_VALUE) return 0;
+        // return ans;
     }
 }
