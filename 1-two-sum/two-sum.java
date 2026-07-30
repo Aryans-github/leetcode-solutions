783d8e1ed021e8f9ee0d4ecd073c4b1a53b7b5e6
Aryan Jumde <aryanjumde2004@gmail.com>
2026-07-30 21:51:39 +0530

    Time: 4 ms (51.94%) | Memory: 46.6 MB (95.79%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,21 +1,36 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int p1=0;
-        int p2=1;
-        int[] ans = new int[2];
-        while(p1<nums.length-1){
-            if(nums[p1]+nums[p2]==target){
-                ans[0]=p1;
-                ans[1]=p2; 
-                break;
-            }
-            if(p2==nums.length-1){
-                p1++;
-                p2=p1+1;
-            } 
-            else p2++;
+        
+        HashMap<Integer,Integer> hs = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hs.put(nums[i],i);
         }
-        return ans;
+        for(int i=0;i<nums.length;i++){
+            if((hs.containsKey(target-nums[i])) && (hs.get(target-nums[i])!=i)){
+                return new int[] {i,hs.get(target-nums[i])};
+            }
+        }
+        return new int[] {0,0};
+        
+        
+        // int p1=0;
+        // int p2=1;
+        // int[] ans = new int[2];
+        // while(p1<nums.length-1){
+        //     if(nums[p1]+nums[p2]==target){
+        //         ans[0]=p1;
+        //         ans[1]=p2; 
+        //         break;
+        //     }
+        //     if(p2==nums.length-1){
+        //         p1++;
+        //         p2=p1+1;
+        //     } 
+        //     else p2++;
+        // }
+        // return ans;
+
+
         // -- Simpliest method using Loop
         //    for(int i=0;i<(nums.length-1);i++){
         //         for(int j=i+1;j<nums.length;j++){
