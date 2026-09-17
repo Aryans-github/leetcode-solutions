0ceee1558e973f257e36e4d91d7aa298f25a85c9
Aryan Jumde <aryanjumde2004@gmail.com>
2026-09-17 17:56:51 +0530

    Time: 632 ms (50.71%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,2 +1,7 @@
-Select c.name AS Customers from Customers c
-where c.id NOT IN (Select customerId from Orders);
+-- Select c.name AS Customers from Customers c
+-- where c.id NOT IN (Select customerId from Orders);
+
+Select c.name as Customers 
+from Customers c left join Orders o
+on c.id=o.customerId 
+where o.customerId is null;
