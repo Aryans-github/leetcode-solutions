c3c5cb4fcba07312620248fcf83a14ea418614bc
Aryan Jumde <aryanjumde2004@gmail.com>
2026-09-19 18:56:11 +0530

    Time: 622 ms (97.22%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,11 +1,20 @@
 # Write your MySQL query statement below
-SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
-FROM Employee e
-JOIN (
-    SELECT departmentId, MAX(salary) AS max_salary
-    FROM Employee
-    GROUP BY departmentId
-) AS max_salaries
-ON e.departmentId = max_salaries.departmentId AND e.salary = max_salaries.max_salary
-JOIN Department d
-ON e.departmentId = d.id;
+-- SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+-- FROM Employee e
+-- JOIN (
+--     SELECT departmentId, MAX(salary) AS max_salary
+--     FROM Employee
+--     GROUP BY departmentId
+-- ) AS max_salaries
+-- ON e.departmentId = max_salaries.departmentId AND e.salary = max_salaries.max_salary
+-- JOIN Department d
+-- ON e.departmentId = d.id;
+
+
+With max_salary as(
+    Select d.id as department_id,Max(e.salary) as max_sal from Employee e join Department d on e.departmentId=d.id group by d.id
+)
+Select d.name as Department,e.name as Employee,e.salary as Salary
+from Employee e join Department d on e.departmentId=d.id 
+join max_salary m on e.departmentId=m.department_id And e.salary=m.max_sal;
+
