6e228f66e5a4daeec7668f2f5d570c5c12f7a249
Aryan Jumde <aryanjumde2004@gmail.com>
2026-09-20 16:35:53 +0530

    Time: 1000 ms (79.29%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,6 +1,15 @@
 # Write your MySQL query statement below
-Select d.name as Department,e.name as Employee,e.salary as Salary
-from Employee e join Department d 
-on e.departmentId=d.id
-where 3 > (select Count(distinct e2.salary) from Employee e2 where e2.departmentId=e.departmentId and e2.salary>e.salary);
+-- Select d.name as Department,e.name as Employee,e.salary as Salary
+-- from Employee e join Department d 
+-- on e.departmentId=d.id
+-- where 3 > (select Count(distinct e2.salary) from Employee e2 where e2.departmentId=e.departmentId and e2.salary>e.salary);
    
+With ranked as(
+    Select id,name,salary,departmentId,
+    dense_rank() over(partition by departmentId order by salary desc) as ranking
+    from Employee
+)
+Select d.name as Department,r.name as Employee,r.salary as Salary
+from ranked r join department d
+on r.departmentId=d.id
+and r.ranking<=3;
